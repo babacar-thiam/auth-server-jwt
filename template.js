705855a27a73edeb4a32c3e5d23802d7d24962c4
Babacar Thiam <bahertaleb@gmail.com>
2022-11-21 10:58:26 +0000

    initial commit

---
+++ template.js
@@ -0,0 +1,13 @@
+export default () => {
+    return `<!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="utf-8">
+          <title>Auth Server JWT</title>
+        </head>
+        <body>
+          <div id="root">Welcome to Auth Server JWT</div>
+          <script type="text/javascript" src="/dist/bundle.js"></script>
+        </body>
+      </html>`;
+  };
